6186a4a1bce32c0fef7cfcd39ad5bbea75a2f5d8
Chase Higgins <chasehx@gmail.com>
2015-05-19 15:04:02 -0600

    make customer mailer, work with tags stuff on blog

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -6,5 +6,10 @@ class ContactMailer < ApplicationMailer
 		mail(:to => "chase@wreet.co", :subject => "New inquiry from: #{@inquiry[:first_name]}");
 	end; # end contact email
 
+	def contact_email_client(inquiry)
+		@inquiry = inquiry;
+		mail(:to => @inquiry[:email], :subject => "Thanks for contacting wreetco");
+	end;
+
 
 end
--- test/mailers/previews/contact_mailer_preview.rb
+++ test/mailers/previews/contact_mailer_preview.rb
@@ -1,6 +1,6 @@
 # Preview all emails at http://localhost:3000/rails/mailers/contact_mailer
 class ContactMailerPreview < ActionMailer::Preview
-	def contact_email_preview
+	def wreetco_email_preview
 		inquiry = {
 			first_name: "Chase",
 			last_name: "Higgins",
@@ -11,4 +11,16 @@ class ContactMailerPreview < ActionMailer::Preview
 		}
 		ContactMailer.contact_email_wreetco(inquiry);
 	end; 
+	
+	def client_email_preview
+		inquiry = {
+			first_name: "Timothy",
+			last_name: "McClient",
+			email: "client@customer.co.uk",
+			subject: "Web Development",
+			submitted: Time.now.to_formatted_s(:long),
+			message: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut ac molestie enim, ut commodo magna. Sed non condimentum augue. Nunc maximus egestas bibendum. Aliquam facilisis ullamcorper ullamcorper. Pellentesque lacus quam, dictum sit amet convallis ut, commodo at massa. Vestibulum tempus risus non finibus bibendum. Vestibulum laoreet augue tellus, sit amet bibendum lorem mattis a. Quisque imperdiet laoreet erat sit amet faucibus."
+		}
+		ContactMailer.contact_email_client(inquiry);
+	end
 end
